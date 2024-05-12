e6c72369c744c5e430f8897a5084ac0b30b5c89c
Nidheesh <nidheeshdevops@gmail.com>
2024-05-12 16:19:17 +0530

    feat: Create player

--- index.js
+++ index.js
@@ -6,3 +6,31 @@ ctx = canvas.getContext('2d')
 ctx.fillStyle = 'black'
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
+class Player {
+    constructor({ position, velocity }) {
+        this.position = position // {x, y}
+        this.velocity = velocity
+    }
+
+    draw() {
+        /* ctx.fillStyle = 'red';
+        ctx.fillRect(this.position.x, this.position.y, 100, 100) */
+        ctx.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false)
+        ctx.fillStyle = 'red';
+        ctx.fill();
+        ctx.moveTo(this.position.x + 30, this.position.y);
+        ctx.lineTo(this.position.x - 10, this.position.y - 10);
+        ctx.lineTo(this.position.x - 10, this.position.y + 10);
+        ctx.closePath()
+
+        ctx.strokeStyle = 'white';
+        ctx.stroke();
+    }
+}
+
+const player = new Player({
+    position: { x: canvas.width / 2, y: canvas.height / 2 },
+    velocity: { x: 0, y: 0 }
+})
+
+player.draw();
